600665b8845f84d8a5021ecc75cfacaf9f079199
killerzero337 <metalmaster37yt@gmail.com>
2024-05-09 18:38:43 +0200

    ajustes en el nuevo componente

--- src/components/Tarjeta.js
+++ src/components/Tarjeta.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Card({ children }) {
   return (
-    <div className="flex flex-col items-center justify-center h-[80vh]">
+    <div className="flex flex-col items-center justify-center h-[100vh] sm:h-[70vh]">
       <div className=" container items-center border-4 border-sky-700 p-8 rounded-md bg-gray-200/80">
         {children}
       </div>
