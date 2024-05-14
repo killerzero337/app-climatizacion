8c0167fd834c4eddaa27a6d94e5b6b962db03cae
killerzero337 <metalmaster37yt@gmail.com>
2024-05-15 01:36:35 +0200

    implementando el componente tarjeta en el resto de rutas

--- src/app/auth/logout/page.js
+++ src/app/auth/logout/page.js
@@ -1,6 +1,7 @@
 import { logout } from "@/lib/actions";
 import { redirect } from "next/navigation";
 import { auth } from "@/auth";
+import Tarjeta from "@/components/Tarjeta";
 
 async function page() {
   const sesion = await auth();
@@ -11,27 +12,27 @@ async function page() {
 
     return (
       <>
-        <div className="flex flex-col items-center justify-center h-[70vh]">
-          <h1 className="text-2xl mb-4">Cerrar sesión</h1>
-          <form className="container flex flex-col items-center gap-4 border-2 border-sky-700 p-8 rounded-md bg-gray-200/80 ">
-            <label className="text-black">
-              Nombre:
+        <Tarjeta>
+          <h1 className="text-2xl mb-4 text-center">¿Desea cerrar sesion?</h1>
+          <form className="container flex flex-col items-center gap-4 text-blue-500">
+            <label className="text-center">
+              Nombre
               <input
                 type="text"
                 name="name"
                 value={user.name}
                 readOnly
-                className="w-full text-black bg-amber-100"
+                className="w-full text-black bg-gray-100 text-center rounded-full"
               />
             </label>
-            <label className="text-black">
-              Email:
+            <label className="text-center">
+              Email
               <input
                 type="email"
                 name="email"
                 value={user.email}
                 readOnly
-                className="w-full text-black bg-amber-100"
+                className="w-full text-black bg-gray-100 text-center rounded-full"
               />
             </label>
             {user.image ? (
@@ -54,7 +55,7 @@ async function page() {
               Cerrar sesion
             </button>
           </form>
-        </div>
+        </Tarjeta>
       </>
     );
   } else {
--- src/components/Tarjeta.js
+++ src/components/Tarjeta.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Card({ children }) {
   return (
-    <div className="flex flex-col items-center justify-center h-[110vh] sm:h-[100vh] text-blue-500 ">
+    <div className="flex flex-col items-center justify-center h-[100vh] sm:h-[100vh] text-blue-500 ">
       <div className=" container items-center border-4 border-sky-400 dark:border-sky-700 p-8 rounded-md bg-gray-200/90 dark:bg-gray-900/90">
         {children}
       </div>
