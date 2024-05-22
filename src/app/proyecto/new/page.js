b6e920689d4859e127c291d2081528cd6b5182e9
killerzero337 <metalmaster37yt@gmail.com>
2024-05-22 21:48:23 +0200

    ...

--- src/app/proyecto/new/page.js
+++ src/app/proyecto/new/page.js
@@ -3,7 +3,7 @@ import FormProyecto from "@/components/Form-proyecto";
 import Tarjeta from "@/components/Tarjeta";
 function page() {
   return (
-    <div className="flex flex-col items-center justify-center h-[200vh] sm:h-[90vh] text-blue-500">
+    <div className="flex flex-col items-center justify-center h-[250vh] sm:h-[80vh] text-blue-500">
       <div className="container items-center border-4 border-sky-400 dark:border-sky-700 p-8 rounded-md bg-gray-200/90 dark:bg-gray-900/90">
         <FormProyecto />
       </div>
