8fc1f36166fcc777c64ebd90b8be1efa9fbda967
killerzero337 <metalmaster37yt@gmail.com>
2024-05-28 21:16:13 +0200

    corrigiendo bug del boton de creacion

--- src/app/proyecto/page.js
+++ src/app/proyecto/page.js
@@ -22,8 +22,8 @@ async function page() {
   } else {
     return (
       <div className="flex flex-col items-center justify-center min-h-screen text-blue-500">
-        <div className="container mx-auto p-8 border-4 border-sky-400 dark:border-sky-700 rounded-md bg-gray-200/90 dark:bg-gray-900/90">
-          <div className="flex justify-center -mb-24">
+        <div className="container mx-auto  p-8 border-4 border-sky-400 dark:border-sky-700 rounded-md bg-gray-200/90 dark:bg-gray-900/90">
+          <div className="flex justify-center mt-24">
             <Link
               href="/proyecto/new"
               className="inline-flex items-center px-5 py-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
