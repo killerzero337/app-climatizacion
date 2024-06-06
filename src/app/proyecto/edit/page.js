788cfd1bac7e34dac82969fc9b24c102d67605a1
killerzero337 <metalmaster37yt@gmail.com>
2024-06-06 23:07:17 +0200

    Añadiendo ayuda en editar tambien

--- src/app/proyecto/edit/page.js
+++ src/app/proyecto/edit/page.js
@@ -3,6 +3,7 @@ import FormProyecto from "@/components/Form-proyecto";
 import { editProyecto, getIdUsuario } from "@/lib/actions-proyecto";
 import { prisma } from "@/lib/prisma";
 import { auth } from "@/auth";
+import Link from "next/link";
 
 export const dynamic = "force-dynamic";
 
@@ -20,6 +21,22 @@ async function page({ searchParams }) {
   return (
     <div className="flex flex-col items-center justify-center -mt-10 h-[450vh] sm:h-[190vh] text-blue-500">
       <div className="container items-center border-4 border-sky-400 dark:border-sky-700 p-8 rounded-md bg-gray-200/90 dark:bg-gray-900/90">
+        <div className="flex flex-col items-center justify-center">
+          <h1 className="text-2xl font-bold text-center p-10">
+            EDITANDO PROYECTO
+          </h1>
+          <div className="flex justify-center align-items-center">
+            <Link
+              href={{
+                pathname: "/proyecto/ayuda",
+              }}
+              target="_blank"
+              className="text-blue-100 animate-pulse"
+            >
+              Si necesita ayuda consulte este enlace.
+            </Link>
+          </div>
+        </div>
         <FormProyecto
           title={"Editar Proyecto"}
           sesion={userId}
