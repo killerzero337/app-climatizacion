dedc92f4349203ef7f58c1c3866f1fdaefd9a1c3
killerzero337 <metalmaster37yt@gmail.com>
2024-06-08 04:14:22 +0200

    ultimos cambios dia de hoy

--- src/app/camara/page.js
+++ src/app/camara/page.js
@@ -12,26 +12,24 @@ async function Page({ searchParams }) {
 
   return (
     <Tarjeta>
-      <div className="flex flex-col items-center justify-center min-h-screen text-blue-500">
-        <div className="container mx-auto p-8 border-4 border-sky-400 dark:border-sky-700 rounded-md bg-gray-200/90 dark:bg-gray-900/90">
-          <div className="flex justify-center mb-6">
-            <Link
-              href="/camara/new"
-              className="inline-flex items-center px-5 py-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-            >
-              Crear Proyecto
-            </Link>
-          </div>
-          <div className="flex flex-wrap gap-5 sm:gap-10 items-center justify-center">
-            {recintos.map((recinto) => (
-              <CamaraTarjeta key={recinto.id} camara={recinto} />
-            ))}
-          </div>
+      <div className="container mx-auto p-8 border-4 border-sky-400 dark:border-sky-700 rounded-md bg-gray-200/90 dark:bg-gray-900/90">
+        <div className="flex justify-center mb-6">
+          <Link
+            href="/camara/new"
+            className="inline-flex items-center px-5 py-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
+            Crear Proyecto
+          </Link>
         </div>
-        <div className="mt-4 p-4 border rounded shadow-md w-full">
-          <div className="flex ">
-            {qv_sens_ocup(proyecto?.temp_ext_ver, proyecto?.hr_ext_ver)}
-          </div>
+        <div className="flex flex-wrap gap-5 sm:gap-10 items-center justify-center">
+          {recintos.map((recinto) => (
+            <CamaraTarjeta key={recinto.id} camara={recinto} />
+          ))}
+        </div>
+      </div>
+      <div className="mt-4 p-4 border rounded shadow-md w-full">
+        <div className="flex ">
+          {qv_sens_ocup(proyecto?.temp_ext_ver, proyecto?.hr_ext_ver)}
         </div>
       </div>
     </Tarjeta>
