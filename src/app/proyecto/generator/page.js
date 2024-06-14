2f0111a1dc4b01e6676a138e97aaae0bf13ab3db
killerzero337 <metalmaster37yt@gmail.com>
2024-06-14 02:42:39 +0200

    Añadiendo las bases de generar un PDF

---
+++ src/app/proyecto/generator/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import Tarjeta from '@/components/Tarjeta'
+function page({ searchParams }) {
+  return (
+    <Tarjeta>Generando PDF...</Tarjeta>
+  )
+}
+
+export default page
--- src/components/tarjetaprisma.js
+++ src/components/tarjetaprisma.js
@@ -53,10 +53,10 @@ async function tarjetaPrisma({ proyecto }) {
               Borrar
             </Link>
             <Link
-              href={`/proyecto/copy/${proyecto.id}`}
+              href={`/proyecto/generator?id=${proyecto?.id}`}
               className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
             >
-              Copiar
+              Exportar
             </Link>
           </div>
         </div>
@@ -101,11 +101,11 @@ async function tarjetaPrisma({ proyecto }) {
               Borrar
             </Link>
             <Link
-              href={`/proyecto/copiar`}
+              href={`/proyecto/generator?id=${proyecto?.id}`}
               query={{ id: proyecto?.id }}
               className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
             >
-              Copiar
+              Exportar
             </Link>
           </div>
         </div>
