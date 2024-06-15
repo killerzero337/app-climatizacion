e275ced10b0f51001622e0a2ac2e955e95a64a5e
killerzero337 <metalmaster37yt@gmail.com>
2024-06-16 01:29:04 +0200

    arreglando bugs en vercel

--- src/components/Tarjeta.js
+++ src/components/Tarjeta.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-function Card({ children }) {
+function Tarjeta({ children }) {
   return (
     <div className="flex flex-col items-center justify-center text-blue-700 dark:text-blue-400">
       <div className="container items-center border-4 border-sky-400 dark:border-sky-700 p-16 rounded-md bg-gray-200/90 dark:bg-gray-900/90">
@@ -10,4 +10,4 @@ function Card({ children }) {
   );
 }
 
-export default Card;
+export default Tarjeta;
